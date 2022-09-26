1541849424a6168765deae9e61ae99851fae6c16
Nikola <tomaha15@gmail.com>
2022-09-27 00:44:24 +0700

    Add playRound function
    function returns string with result of the round

--- script.js
+++ script.js
@@ -20,5 +20,18 @@ let getComputerChoice = function(min, max) {
   }
   console.log(compChoice(choice));
 
-  let computerSelection = compChoice(choice);
+  let computerSelection = compChoice(choice); // String that is either `Rock`, `Paper` or `Scissors`
   console.log(computerSelection);
+
+  function playRound(playerSelection, computerSelection) {
+    if(playerSelection == `Rock` && computerSelection == `Scissors`) {return `You win! Rock beats Scissors`;}
+    else if(playerSelection == `Paper`&& computerSelection == `Rock`) {return `You win! Paper beats Rock`;}
+    else if(playerSelection == `Scissors`&& computerSelection == `Paper`) {return `You win! Scissors beat Rock`;}
+    else if(playerSelection == `Rock`&& computerSelection == `Paper`) {return `You lose! Paper beats Rock`;}
+    else if(playerSelection == `Paper`&& computerSelection == `Scissors`) {return `You lose! Scissors beat Paper`;}
+    else if(playerSelection == `Scissors`&& computerSelection == `Rock`) {return `You lose! Rock beats Scissors`;}
+    else {return `Draw! Try again`}
+  }
+  
+  const playerSelection = `Rock`;
+  console.log(playRound(playerSelection, computerSelection));
