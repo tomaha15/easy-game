ce538061526c823a21a233ff2d30b0ede187569c
Nikola <tomaha15@gmail.com>
2022-09-27 00:12:26 +0700

    Add computerSelection variable that is assigned random choice
    function compChoice passes a choice variable that is a random number and based on that returnes string result

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+
 let getComputerChoice = function(min, max) {
     min = Math.ceil(1);
     max = Math.floor(3);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
   } 
-console.log(getComputerChoice()); // Returns random number between 1 and 3
 
+  let choice = getComputerChoice();
+
+
+  function compChoice(choice) {
+    let result;
+
+    if(choice < 2) {
+      result = `Rock`;}
+      else if(choice > 2) {result = `Paper`;}
+      else {result = `Scissors`;}
+
+    return result;  
+  }
+  console.log(compChoice(choice));
+
+  let computerSelection = compChoice(choice);
+  console.log(computerSelection);
